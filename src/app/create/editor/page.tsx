ba2519e760bd55a11f21b409c7516adfd555d8d1
Arda Sarı <ardasarico@gmail.com>
2025-02-20 12:24:50 +0300

    update

--- src/app/create/editor/page.tsx
+++ src/app/create/editor/page.tsx
@@ -1,12 +1,19 @@
 import CodeEditor from '@/components/editor/CodeEditor';
 import Renderer from '@/components/editor/Renderer';
+import Button from '@/components/ui/Button';
 
 export default function EditorPage() {
   return (
     <div className={'flex h-screen min-h-fit w-full'}>
       <CodeEditor />
-      <div className={'flex h-full w-full items-center justify-center gap-5'}>
+      <div className={'flex h-full w-full flex-col items-center justify-center gap-5 p-6'}>
         <Renderer />
+        <div className={'flex items-center gap-2'}>
+          <Button variant={'secondary'} size={'md'}>
+            Back to Edit
+          </Button>
+          <Button size={'md'}>Next Step</Button>
+        </div>
       </div>
     </div>
   );
--- src/components/editor/Renderer/index.tsx
+++ src/components/editor/Renderer/index.tsx
@@ -1,5 +1,11 @@
 const Renderer = () => {
-  return <div className={'h-32 w-32 bg-neutral-600'}>renderer</div>;
+  return (
+    <div className={'aspect-square w-full max-w-[600px] rounded-[24px] border border-neutral-500 bg-neutral-600 p-3'}>
+      <div className={'flex h-full w-full items-center justify-center rounded-[12px] bg-neutral-700'}>
+        <p>Your generation will be shown here when you compile</p>
+      </div>
+    </div>
+  );
 };
 
 export default Renderer;
