e23fd5925da931c084ea8e711f6f70ba0f0d52f6
Arda Sarı <ardasarico@gmail.com>
2025-02-21 15:28:36 +0300

    Customize page

--- src/app/create/(others)/customize/page.tsx
+++ src/app/create/(others)/customize/page.tsx
@@ -2,6 +2,9 @@
 
 import NftCard from '@/components/NftCard';
 import ImageInput from '@/components/ui/ImageInput';
+import TextInput from '@/components/ui/TextInput';
+import Button from '@/components/ui/Button';
+import NumberInput from '@/components/ui/NumberInput';
 
 export default function CustomizePage() {
   const handleUpload = (file: File | null) => {
@@ -16,8 +19,17 @@ export default function CustomizePage() {
         <p className={'font-medium text-neutral-300'}>Once your collection is minted you will not be able to change any of its information.</p>
       </div>
       <div className={'flex h-full w-full items-center gap-8'}>
-        <div className={'w-full'}>
-          <ImageInput onUpload={handleUpload} />
+        <div className={'flex w-full items-center justify-center p-6'}>
+          <div className={'flex max-w-[520px] flex-col gap-8'}>
+            <ImageInput onUpload={handleUpload} />
+            <TextInput label="Title" type="text" placeholder="Enter title here" />
+            <TextInput label="Description" placeholder="Tell us all about your collection!" />
+            <div className={'flex items-center gap-8'}>
+              <NumberInput step={1} label="Price" max={999} min={0} />
+              <NumberInput step={10} label="Supply" max={999} min={0} />
+            </div>
+            <Button>Next Setup</Button>
+          </div>
         </div>
         <div className={'flex w-full items-center justify-center'}>
           <NftCard />
