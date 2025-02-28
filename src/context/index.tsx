e28588711c1da7ddd9be3298f392b67f79e6ee47
Arda Sarı <ardasarico@gmail.com>
2025-02-28 18:26:08 +0300

    update

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -3,7 +3,7 @@
 import { wagmiAdapter, projectId } from '@/config';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createAppKit } from '@reown/appkit/react';
-import { mainnet, arbitrum, avalanche, base, optimism, polygon } from '@reown/appkit/networks';
+import { mainnet, forma } from '@reown/appkit/networks';
 import React, { type ReactNode } from 'react';
 import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi';
 
@@ -23,8 +23,8 @@ const metadata = {
 const modal = createAppKit({
   adapters: [wagmiAdapter],
   projectId,
-  networks: [mainnet, arbitrum, avalanche, base, optimism, polygon],
-  defaultNetwork: mainnet,
+  networks: [mainnet, forma],
+  defaultNetwork: forma,
   metadata: metadata,
   features: {
     analytics: true,
