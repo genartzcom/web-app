cd0d03c80704bb8f863a9e395d1095b8a10e82ea
Arda Sarı <ardasarico@gmail.com>
2025-02-28 22:05:28 +0300

    Update layout.tsx

--- src/app/create/(others)/layout.tsx
+++ src/app/create/(others)/layout.tsx
@@ -9,7 +9,9 @@ export default function Layout({
     <div className={'flex h-screen flex-col items-center'}>
       <div className={'h-18 w-[calc(100%-192px)] max-w-[1400px] border-x border-x-neutral-600'}></div>
       <Divider />
-      <div className={'relative h-full w-[calc(100%-192px)] max-w-[1400px] overflow-hidden border-x border-x-neutral-600 p-8'}>{children}</div>
+      <div className={'relative h-full min-h-fit w-[calc(100%-192px)] max-w-[1400px] overflow-hidden border-x border-x-neutral-600 p-8'}>
+        {children}
+      </div>
       <Divider />
       <div className={'h-18 w-[calc(100%-192px)] max-w-[1400px] border-x border-x-neutral-600'}></div>
     </div>
